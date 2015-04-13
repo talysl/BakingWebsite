339b76915e6dfe0c28c5e8457d74b5ed6bccbb85
talysl <talitasilvalima88@gmail.com>
2015-04-13 15:20:15 +0100

    Update v5 - style, home, recipes, new pages

---
+++ mixture/mixture.js
@@ -0,0 +1,14 @@
+$("#div1").hide();
+$("#toggle1").click(function(){           
+    $("#div1").slideToggle();
+});
+
+$("#div2").hide();
+$("#toggle2").click(function(){           
+    $("#div2").slideToggle();
+});
+
+$("#div3").hide();
+$("#toggle3").click(function(){           
+    $("#div3").slideToggle();
+});
